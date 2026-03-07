ef3c5aee0f0408fe1e9b62e8ecf0e56ce5572911
sohee52 <wneh2023@naver.com>
2026-03-07 12:22:09 +0900

    solve: 03_5월 식품들의 총매출 조회하기

---
+++ programmers/join/03_5월 식품들의 총매출 조회하기.sql
@@ -0,0 +1,7 @@
+select p.product_id, p.product_name, p.price * sum(o.amount) as total_sales
+from food_product as p
+join food_order as o
+on p.product_id = o.product_id
+where date_format(o.produce_date, "%Y-%m") = "2022-05"
+group by p.product_id
+order by total_sales desc, p.product_id;
