83c0249d8d943758c94eff4d6a02bd5d8b59b7bd
sohee52 <wneh2023@naver.com>
2026-03-07 13:40:53 +0900

    solve: 11_상품을 구매한 회원 비율 구하기

---
+++ programmers/join/11_상품을 구매한 회원 비율 구하기.sql
@@ -0,0 +1,19 @@
+select
+    year(o.sales_date) as year,
+    month(o.sales_date) as month,
+    count(distinct o.user_id) as purchased_users,
+    round(
+        count(distinct o.user_id) / 
+        (
+            select count(*)
+            from user_info
+            where year(joined) = 2021
+        ),
+        1
+        ) as purchased_ratio
+from user_info as u
+join online_sale as o
+on u.user_id = o.user_id
+where year(u.joined) = 2021
+group by year(o.sales_date), month(o.sales_date)
+order by year, month;
